3204bf9db56e582f979f476f0e550038cbf64436
BIBAY-JOHNTEX-BSCS32E1 <161927922+JohnTexBibay@users.noreply.github.com>
2024-06-15 17:45:47 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_BIBAY_JOHNTEX_BSCS_32E.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
